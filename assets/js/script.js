52590213fe857e74d41d2e6e82a9b3546e07e945
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-21 13:34:26 +0300

    update list URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,16 @@ $(document).ready(function() {
 
 	$(".newsletter-signup").ajaxChimp({
 		callback: mailchimpResponse,
-		url: "http://sandlabs.us9.list-manage.com/subscribe/post?u=a6801279da611bc55645c0413&id=9977eec545" // Replace your mailchimp post url inside double quote "".  
+		url: "http://creap.us9.list-manage.com/subscribe?u=6a9d144cf8d024b24d643696e&id=f6738e96de" // Replace your mailchimp post url inside double quote "".  
 	});
 
 	function mailchimpResponse(resp) {
 		 if(resp.result === 'success') {
-		 
+
 			$('.alert-success').html(resp.msg).fadeIn().delay(3000).fadeOut();
-			
+
 		} else if(resp.result === 'error') {
 			$('.alert-warning').html(resp.msg).fadeIn().delay(3000).fadeOut();
-		}  
+		}
 	};
  });
